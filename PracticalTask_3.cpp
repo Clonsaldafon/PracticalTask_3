201bd7bbdfa26b584a22bdf702685db650c2ada6
Anton Bezmelnitsin <a.bezmelnitsin.a@yandex.ru>
2023-03-22 09:13:35 +0500

    Ready v0.2

--- PracticalTask_3.cpp
+++ PracticalTask_3.cpp
@@ -2,5 +2,36 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	unsigned char a, b, x;
+	char y;
+	bool c = 0, d = 0;
+	scanf_s("%hhu %hhu", &a, &b);
+
+	_asm
+	{
+		mov al,a
+		mov bl,b
+		add al,bl
+		jno sign
+		mov c,1
+	sign:
+		jnc unsign
+		mov d,1
+	unsign:
+		mov y,al
+		mov x,al
+	}
+
+	printf("\na + b = %d (signed)\n", y);
+	printf("a + b = %hhu (unsigned)\n", x);
+
+	if (c == 1)
+		printf("overflow (signed)\n");
+	else
+		printf("no overflow (signed)\n");
+
+	if (d == 1)
+		printf("overflow (unsigned)\n");
+	else
+		printf("no overflow (unsigned)\n");
 }
